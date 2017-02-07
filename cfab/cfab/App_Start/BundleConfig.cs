21a398abe462f7e631e91c0b9caefce458aafea4
aliciabigley <aliciabigley@gmail.com>
2017-02-07 12:29:13 -0600

    styling site

--- cfab/cfab/App_Start/BundleConfig.cs
+++ cfab/cfab/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace cfab
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-cosmo.css",
+                      "~/Content/bootstrap-simplex.css",
                       "~/Content/site.css"));
         }
     }
