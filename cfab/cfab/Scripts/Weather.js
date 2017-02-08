9ee6fe30432449ebbf31716e080d58ea1d0e6ea4
FrankFranken <Frankfranken1985@gmail.com>
2017-02-08 12:13:11 -0600

    finished weather

--- cfab/cfab/Scripts/Weather.js
+++ cfab/cfab/Scripts/Weather.js
@@ -1,4 +1,5 @@
-﻿getWeather: function() {
+﻿weather();
+function weather() {
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/forecast?q=milwaukee,us&appid=da80198220685bc49e1e8df5738ba178',
         type: 'GET',
@@ -6,21 +7,19 @@
         success: function (data) {
             var wrapper = $("#WeatherWrapper");
             wrapper.empty();
-            wrapper.append(mainWeather.createWeatherWidg(data));
+            wrapper.append(printData(data));
         },
-        error: function() {
-            alert('Failed!');
+        error: function () {
+            console.log('Failed!');
         }
- 
     });
 }
- 
-//Prints result from the weatherapi, receiving as param an object
-createWeatherWidg: function(data) {
-    return "<div class='pressure'> <p>Temperature: " + (data.main.temp - 273.15).toFixed(2) + " C</p></div>"+
-            "<div class='description'> <p>Title: " + data.weather[0].main + "</p></div>" +
-            "<div class='description'> <p>Description: " + data.weather[0].description + "</p></div>" +
-            "<div class='wind'> <p>Wind Speed: " + data.wind.speed + "</p></div>" +
-            "<div class='humidity'> <p>Humidity: " + data.main.humidity + "%</p></div>" +
-            "<div class='pressure'> <p>Pressure: " + data.main.pressure + " hpa</p></div>";
+
+function printData(data) {
+    for (var i = 0; i < 40; i+=8) {
+        $('#showResults').append('<tr><td>' + data.list[i].dt_txt.split(/[" "]/)[0].replace("2017-", "") + '</td>'
+        + '<td>' + data.city.name + '</td>'
+        + '<td>' + Math.round(data.list[i].main.temp - 242) + '</td>'
+        + '<td>' + data.list[i].weather[0].description + ' </td>')};
+        console.log(data);
 }
