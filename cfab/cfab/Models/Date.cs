c324d953282aed1491f82f72290ab700fd74401a
aliciabigley <aliciabigley@gmail.com>
2017-02-08 14:41:34 -0600

    change main date form names

--- cfab/cfab/Models/Date.cs
+++ cfab/cfab/Models/Date.cs
@@ -10,41 +10,47 @@ namespace cfab.Models
     {
         public int Id { get; set; }
 
+        [Display(Name ="Name your Date")]
         public string DateName { get; set; }
 
+        [Display(Name = "Date of Date")]
         public DateTime DayOfDate { get; set;}
 
         public int UserId { get; set; }
 
+        [Display(Name = "Primary Address")]
         public string StreetOne { get; set; }
 
+        [Display(Name = "Secondary Address")]
         public string StreetTwo { get; set; }
 
+        [Display(Name = "Zip Code")]
         public Zipcode Zipcode { get; set; }
 
         public int ZipcodeId { get; set; }
 
-        [Display(Name = "Will you be going to Eat?")]
+        [Display(Name = "Will you be going to eat?")]
         public bool willEat { get; set; }
 
-        [Display(Name = "Are they are Chatty")]
+        [Display(Name = "Are they Chatty")]
         public bool IsChatty { get; set; }
 
-        [Display(Name = "Are they are Active")]
+        [Display(Name = "Are they Active")]
         public bool IsActive { get; set; }
 
-        [Display(Name = "Are they are Artsy")]
+        [Display(Name = "Are they Artsy")]
         public bool IsArtsy { get; set; }
 
-
+        [Display(Name = "Price Range")]
         public RestaurantType RestaurantType { get; set; }
 
+        [Display(Name = "Atmosphere")]
         public int RestaurantTypeId { get; set; }
 
-
+        [Display(Name = "Seach by Keyword")]
         public string Keyword { get; set; }
 
-
+        [Display(Name = "Time of Day")]
         public DateTimeOfDay DateTimeOfDay { get; set; }
 
         public int DateTimeOfDayId { get; set; }
